8f69088344caf98162bed6ae4086c16db4ef5146
DanielAzeved0 <Daniel.azevedo081205@gmail.com>
2025-04-26 16:35:38 -0300

    Mais algumas modificações na automação

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,4 +1,12 @@
 /// <reference types="cypress"/>
+describe('US-001-Funcionladidade: Busca de Filmes', () => {
+  it('Deve fazer o cadastro de campos obrigatorios', () => {
+    cy.visit('http://127.0.0.1:8080/')
+    cy.get('#search-input').type('The Godfather')
+    cy.get('#search-button').click()
+    cy.get('#clear-button')//.cliclk()
+  })
+})
 
 describe('US-012-Funcionalidade; Cadastro de membros', () => {
   it('Deve fazer o cadastro de campos obrigatorios', () => {
@@ -9,5 +17,6 @@ describe('US-012-Funcionalidade; Cadastro de membros', () => {
     cy.get('#signup-phone').type('11969714929')
     cy.get('#signup-password').type('Daga1011@')
     cy.get('#signup-button').click()
+    cy.get('#signup-response').should('contain' , 'Cadastro realizado com sucesso!')
   })
 })
